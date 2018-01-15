70dd48d29d02cb8cd7bef7f559957bf6bbbecd9c
Заремба Александр <spbligr@gmail.com>
2018-01-15 14:52:18 +0300

    recurs func

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io"
+	//"io"
 	"os"
+	//"path/filepath"
+	//"strings"
 	"path/filepath"
 	"strings"
 )
+
+var disabledFound = []string{".git", ".gitignore", ".idea", "README.md"}
+
 
 func main() {
 	out := os.Stdout
@@ -19,4 +24,35 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+}
+
+func dirTree(out interface{}, filePath string, printFiles bool) error  {
+	err := filepath.Walk(filePath, func(path string, f os.FileInfo, err error) error {
+		if isDisabled(path) {
+			return nil
+		}
+
+
+		fmt.Println(path)
+
+		//fileList = append(fileList, path)
+		return nil
+	})
+
+	//for _, file := range fileList {
+	//	fmt.Println(file)
+	//}
+
+	return err
+}
+
+func isDisabled(path string) bool{
+	pathList := strings.Split(path, `\`)
+
+	for _, value := range disabledFound {
+		if pathList[0] == value {
+			return true
+		}
+	}
+	return false
 }
